e7403589f83d7fea73869188ba2350c69d084ddb
Nggadas <enggadas80@gmail.com>
2017-09-23 22:00:45 +0100

    Removing names from the list

--- src/App.js
+++ src/App.js
@@ -5,34 +5,87 @@ import GuestList from "./Guestlist";
 class App extends Component {
 
     state = {
+        isFiltered: false,
+        pendingGuest: "",
         guests: [
             {
                 name: 'Treasure',
-                isConfirmed: false
+                isConfirmed: false,
+                isEditing: false
             },
             {
                 name: 'Nick',
-                isConfirmed: true
+                isConfirmed: true,
+                isEditing: false
             },
             {
                 name: 'Matt K',
-                isConfirmed: false
+                isConfirmed: false,
+                isEditing: true
             }
         ]
-    }
+    };
 
-    toggleConfirmationAt = indexToChange =>
+    toggleGuestPropertyAt = (property, indexToChange) =>
         this.setState({
            guests: this.state.guests.map((guest, index) => {
                if (index === indexToChange) {
                    return{
                        ...guest,
-                       isConfirmed: !guest.isConfirmed
+                       [property]: !guest[property]
                    }
                }
                return guest;
            })
         });
+
+    toggleConfirmationAt = index =>
+        this.toggleGuestPropertyAt("isConfirmed", index);
+
+    removeGuestAt = index =>
+        this.setState({
+            guests: [
+                ...this.state.guests.slice(0, index),
+                ...this.state.guests.slice(index + 1),
+            ]
+        });
+
+    toggleEditingAt = index =>
+        this.toggleGuestPropertyAt("isEditing", index);
+
+    setNameAt = (name, indexToChange) =>
+        this.setState({
+            guests: this.state.guests.map((guest, index) => {
+                if (index === indexToChange) {
+                    return{
+                        ...guest,
+                        name
+                    }
+                }
+                return guest;
+            })
+        });
+
+    toggleFilter = () =>
+        this.setState({ isFiltered: !this.state.isFiltered });
+
+    handleNameInput = e =>
+        this.setState({ pendingGuest:e.target.value });
+
+    newGuestSubmitHandler = e => {
+        e.preventDefault();
+        this.setState({
+            guests:[
+                {
+                    name: this.state.pendingGuest,
+                    isConfirmed: false,
+                    isEditing: false
+                },
+                ...this.state.guests
+            ],
+            pendingGuest: ''
+        })
+    };
 
     getTotalInvited = () => this.state.guests.length;
     //getAttendingGuests = () =>
@@ -45,8 +98,11 @@ class App extends Component {
                 <header>
                     <h1>RSVP</h1>
                     <p>A Treehouse App</p>
-                    <form>
-                        <input type="text" value="Safia" placeholder="Invite Someone" />
+                    <form onSubmit={this.newGuestSubmitHandler}>
+                        <input type="text"
+                               onChange={this.handleNameInput}
+                               value={this.state.pendingGuest}
+                               placeholder="Invite Someone" />
                             <button type="submit" name="submit" value="submit">Submit</button>
                     </form>
                 </header>
@@ -55,7 +111,9 @@ class App extends Component {
                     <div>
                         <h2>Invitees</h2>
                         <label>
-                            <input type="checkbox" /> Hide those who haven't responded
+                            <input type="checkbox" onChange={this.toggleFilter}
+                            checked={this.state.isFiltered}/>
+                            Hide those who haven't responded
                         </label>
                     </div>
                     <table className
@@ -78,7 +136,12 @@ class App extends Component {
 
                     <GuestList
                         guests={this.state.guests}
-                        toggleConfirmationAt={this.toggleConfirmationAt()} />
+                        toggleConfirmationAt={this.toggleConfirmationAt}
+                        toggleEditingAt={this.toggleEditingAt}
+                        setNameAt={this.setNameAt}
+                        isFiltered={this.state.isFiltered}
+                        removeGuestAt={this.removeGuestAt}
+                    />
 
                 </div>
             </div>
